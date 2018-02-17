0e0e374a3f907b57c3c5a90d823c02da2620d17c
anettepaula <35691389+anettepaula@users.noreply.github.com>
2018-02-17 18:12:39 +0100

    Add files via upload

--- bucle_for07A.py
+++ bucle_for07A.py
@@ -1,9 +1,2 @@
 #coding: utf-8
-print "                  SUMA ENTRE VALORES"
-x = input("Esriba un número entero: ")
-cont = 0
-num = input("Escribe un número mayor que "+str(x)+": ")
-while num<=x :
-	print "¡Le he pedido un número mayor que "+str(x)+"!"
-	num = input("Escribe un número mayor que "+str(x)+": ")
-for i in range(
+
